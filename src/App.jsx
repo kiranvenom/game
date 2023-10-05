c61507b63dfbcb0277653e379a1c7efb98a3e12c
Kiran <km060360@gmail.com>
2023-10-05 15:57:32 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from '@chakra-ui/react';
+
+const App = () => {
+	return (
+		<Button variant={'outline'} colorScheme="blue">
+			Button
+		</Button>
+	);
+};
+
+export default App;
